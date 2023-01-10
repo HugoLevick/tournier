0549beebccbbc9ed1e01844ca140df6cd149a6d5
HugoLevick <hlals12346@gmail.com>
2023-01-10 10:49:25 -0600

    Coming Soon...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+
+  logger.log('App running on port ' + process.env.PORT);
 }
 bootstrap();
