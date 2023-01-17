0557d63b04af499f1b93d53dc14b72f1981b25eb
HugoLevick <hlals12346@gmail.com>
2023-01-17 17:26:43 -0600

    Log in button fix v2

--- public/js/getUserByJwt.js
+++ public/js/getUserByJwt.js
@@ -17,11 +17,11 @@ async function getUserByJwt() {
     if (loginAnchor && user) {
       loginAnchor.href = '/auth/logout.html';
       loginAnchor.innerHTML = `Heya, ${user.twitchUsername}!`;
-    } else {
-      if (!user) {
-        loginAnchor.href = `https://id.twitch.tv/oauth2/authorize?client_id=cygujur0ps52ov7fguxb8tix06wa7z&redirect_uri=${location.origin}/auth/twitch-oauth.html&response_type=code&scope=user:read:email`;
-      }
     }
+  }
+
+  if (!user) {
+    loginAnchor.href = `https://id.twitch.tv/oauth2/authorize?client_id=cygujur0ps52ov7fguxb8tix06wa7z&redirect_uri=${location.origin}/auth/twitch-oauth.html&response_type=code&scope=user:read:email`;
   }
 
   console.log({ user });
